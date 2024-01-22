7c4c8e44464266a91e0d46aa80a6c052827b5a74
lucaFiscariello <80633764+lucaFiscariello@users.noreply.github.com>
2024-01-22 11:12:56 +0000

    configurazione mqtt ok

--- repo-service/mqtt-publisher/start.sh
+++ repo-service/mqtt-publisher/start.sh
@@ -1,8 +1,9 @@
-
 sudo apt-get update -y
 sudo apt-get install mosquitto mosquitto-clients -y
+sleep 3
 echo -e "allow_anonymous true\nlistener 1883" | sudo tee -a /etc/mosquitto/mosquitto.conf
 sudo service mosquitto restart
 
+sleep 3
 sudo docker build -t mqtt-publisher /home/ubuntu/marketplace/repo-service/mqtt-publisher/
 sudo docker run -it  --network=bridge --name mqtt-publisher  mqtt-publisher
--- repo-service/mqtt-publisher/stop.sh
+++ repo-service/mqtt-publisher/stop.sh
@@ -1,3 +1,5 @@
 sudo docker stop mqtt-publisher
 sudo docker rm mqtt-publisher
+sudo apt-get purge mosquitto mosquitto-clients -y
+sudo apt-get autoremove -y
 
